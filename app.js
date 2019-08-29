ad4a8e45e266b1c7e1c6620aa842818479379218
Joachimzeelmaekers <joa.zeelmaekers@hotmail.com>
2019-08-29 12:34:26 +0200

    implemented all calls to send, delete and create a campaign.

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const bodyParser = require('body-parser');
 const repository = require('./repository');
 const cors = require('cors');
 
-const { createNewsLetter } = require('./html-renderer/NewsLetter')
-const { getNewsItem } = require('./html-renderer/NewsItem')
+const { createNewsLetter } = require('./html-renderer/NewsLetter');
+const { getNewsItem } = require('./html-renderer/NewsItem');
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -15,13 +15,16 @@ dotenv.config();
 const Mailchimp = require('mailchimp-api-v3');
 const mailchimp = new Mailchimp(process.env.MAILCHIMP_API);
 const moment = require('moment');
-moment.locale("nl");
+const fromName = 'Kaleidos';
+const replyTo = 'joachim.zeelmaekers@craftworkz.be';
+
+moment.locale('nl');
 
 app.use(bodyParser.json({ type: 'application/*+json' }));
 app.use(cors());
 
-app.post('/mails', (req, res) => {
-  return sendNewsletter(req, res);
+app.post('/createCampaign', (req, res) => {
+  return createCampagne(req, res);
 });
 
 app.get('/', (req, res) => {
@@ -29,22 +32,19 @@ app.get('/', (req, res) => {
 });
 
 const getMostRecentNewsletter = async (req, res) => {
-
   try {
-
     let response = await repository.getAgendaWhereisMostRecentAndFinal();
     const { agenda_uuid } = response[0] || {};
 
     if (!agenda_uuid) {
       res.send({ status: ok, statusCode: 500, newsletter: [] });
     } else {
-
       let newsletter = await repository.getNewsLetterByAgendaId(agenda_uuid);
       if (!newsletter) {
-        throw new Error("no newsletters present");
+        throw new Error('no newsletters present');
       }
 
-      newsletter = newsletter.map(newsletter_item => {
+      newsletter = newsletter.map((newsletter_item) => {
         let item = {};
         item.id = newsletter_item.uuid;
         item.webtitle = newsletter_item.title;
@@ -52,11 +52,11 @@ const getMostRecentNewsletter = async (req, res) => {
         item.body = newsletter_item.text;
         item.publication_date = newsletter_item.created;
         item.modification_date = newsletter_item.modified;
-        item.type = "agenda_item";
+        item.type = 'agenda_item';
         if (item.remark) {
-          item.agenda_item_type = "Opmerking"
+          item.agenda_item_type = 'Opmerking';
         } else {
-          item.agenda_item_type = "Beslissing"
+          item.agenda_item_type = 'Beslissing';
         }
         return item;
       });
@@ -64,40 +64,34 @@ const getMostRecentNewsletter = async (req, res) => {
       res.send({
         total: newsletter.length,
         size: newsletter.length,
-        items: newsletter
-      })
-
+        items: newsletter,
+      });
     }
-
-
-
   } catch (error) {
     console.error(error);
     res.send({ status: ok, statusCode: 500, body: { error } });
   }
 };
 
-const sendNewsletter = async (req, res) => {
-
+const createCampagne = async (req, res) => {
   try {
-
     const agendaId = req.query.agendaId;
     if (!agendaId) {
-      throw new Error("Request parameter agendaId can not be null");
+      throw new Error('Request parameter agendaId can not be null');
     }
 
     let newsletter = await repository.getNewsLetterByAgendaId(agendaId);
     if (!newsletter) {
-      throw new Error("no newsletters present");
+      throw new Error('no newsletters present');
     }
 
-    const planned_start = moment(newsletter[0].planned_start).format("dddd DD-MM-YYYY");
-    const news_items_HTML = await newsletter.map(item => getNewsItem(item));
+    const planned_start = moment(newsletter[0].planned_start).format('dddd DD-MM-YYYY');
+    const news_items_HTML = await newsletter.map((item) => getNewsItem(item));
     let html = await createNewsLetter(news_items_HTML, planned_start);
 
     const template = {
-      "name": `Nieuwsbrief ${planned_start}`,
-      html
+      name: `Nieuwsbrief ${planned_start}`,
+      html,
     };
 
     const created_template = await mailchimp.post({
@@ -107,29 +101,63 @@ const sendNewsletter = async (req, res) => {
 
     const { id } = created_template;
     const campaign = {
-      "type": "regular",
-      "recipients": {
-        "list_id": "51ca8b6b84"
+      type: 'regular',
+      recipients: {
+        list_id: '5480352579',
       },
-      "settings": {
-        "subject_line": `Nieuwsbrief ${planned_start}`,
-        "preview_text": "",
-        "title": `Nieuwsbrief ${planned_start}`,
-        "from_name": "Tom Ploem",
-        "reply_to": "info@liskdiscovery.com",
-        "inline_css": true,
-        "template_id": id
-      }
+      settings: {
+        subject_line: `Nieuwsbrief ${planned_start}`,
+        preview_text: '',
+        title: `Nieuwsbrief ${planned_start}`,
+        from_name: fromName,
+        reply_to: replyTo,
+        inline_css: true,
+        template_id: id,
+      },
     };
-    await mailchimp.post({
+
+    const createdCampagne = await mailchimp.post({
       path: '/campaigns',
       body: campaign,
     });
 
-    res.send({ campaign })
+    const { web_id, archive_url } = createdCampagne;
 
+    res.send({
+      status: ok,
+      statusCode: 200,
+      body: {
+        campaign_id: createdCampagne.id,
+        campaign_web_id: web_id,
+        archive_url,
+      },
+    });
   } catch (error) {
     console.error(error);
     res.send({ status: ok, statusCode: 500, body: { error } });
   }
 };
+
+app.delete('/deleteCampaign/:id', async (req, res) => {
+  const campaign_id = req.params.id;
+  if (!campaign_id) {
+    throw new Error('Request parameter campaign_id can not be null');
+  }
+  const deleted = await deleteCampaign(campaign_id);
+  res.send({ deleted });
+});
+
+const deleteCampaign = (id) => {
+  return mailchimp.delete({
+    path: `/campaigns/${id}`,
+  });
+};
+
+app.post('/sendCampaign/:id', async (req, res) => {
+  const campaign_id = req.params.id;
+  const sendCampaign = await mailchimp.post({
+    path: `/campaigns/${campaign_id}/actions/send`,
+  });
+
+  res.send({ sendCampaign });
+});
--- repository/index.js
+++ repository/index.js
@@ -1,8 +1,7 @@
 import mu from 'mu';
 
 const getAgendaWhereisMostRecentAndFinal = async () => {
-
-    const query = `
+  const query = `
         PREFIX dct: <http://purl.org/dc/terms/>
         PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
         PREFIX besluitvorming: <http://data.vlaanderen.be/ns/besluitvorming#>
@@ -23,12 +22,12 @@ const getAgendaWhereisMostRecentAndFinal = async () => {
             ?agenda mu:uuid ?agenda_uuid .
           }
         } ORDER BY DESC(?date) DESC(?agenda_date) LIMIT 1`;
-    let data = await mu.query(query);
-    return parseSparqlResults(data);
+  let data = await mu.query(query);
+  return parseSparqlResults(data);
 };
 
 const getNewsLetterByAgendaId = async (agendaId) => {
-    const query = `
+  const query = `
         PREFIX dct: <http://purl.org/dc/terms/>
         PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
         PREFIX besluitvorming: <http://data.vlaanderen.be/ns/besluitvorming#>
@@ -38,48 +37,44 @@ const getNewsLetterByAgendaId = async (agendaId) => {
         PREFIX dct: <http://purl.org/dc/terms/>
         PREFIX prov: <http://www.w3.org/ns/prov#>
         
-        SELECT ?uuid ?title ?richtext ?text ?subtitle ?publication_date ?remark ?created ?modified WHERE {
+        SELECT ?uuid ?title ?richtext ?text ?subtitle ?publication_date ?remark ?created ?modified ?proposal ?newsletterRemark WHERE {
             GRAPH <http://mu.semte.ch/graphs/organizations/kanselarij> {
                <http://kanselarij.vo.data.gift/id/agendas/${agendaId}> dct:hasPart ?agendaitem . 
-               ?agendaitem prov:generated  ?newsletter . 
+               ?subcase besluitvorming:isGeagendeerdVia ?agendaitem .
+               ?subcase prov:generated  ?newsletter . 
                OPTIONAL { ?agendaitem ext:wordtGetoondAlsMededeling ?remark . }
                OPTIONAL { ?agendaitem besluitvorming:aanmaakdatum ?created . }
                OPTIONAL { ?agendaitem ext:modified ?modified . }
                OPTIONAL { ?newsletter mu:uuid ?uuid . }
                OPTIONAL { ?newsletter besluitvorming:inhoud ?text . }
                OPTIONAL { ?newsletter ext:htmlInhoud ?richtext . }
+               OPTIONAL { ?newsletter ext:voorstelVan ?proposal . }
                OPTIONAL { ?newsletter dbpedia:subtitle ?subtitle . }
                OPTIONAL { ?newsletter dct:title ?title . }
                OPTIONAL { ?newsletter dct:issued ?publication_date . }
              }
         }`;
-    let data = await mu.query(query);
-    return parseSparqlResults(data);
+  let data = await mu.query(query);
+  return parseSparqlResults(data);
 };
-
 
 const parseSparqlResults = (data) => {
-    const vars = data.head.vars;
-    return data.results.bindings.map(binding => {
-        let obj = {};
+  const vars = data.head.vars;
+  return data.results.bindings.map((binding) => {
+    let obj = {};
 
-        vars.forEach(varKey => {
-            if (binding[varKey]){
-                obj[varKey] = binding[varKey].value;
-            }else {
-                obj[varKey] = null;
-            }
-        });
-        return obj;
-    })
+    vars.forEach((varKey) => {
+      if (binding[varKey]) {
+        obj[varKey] = binding[varKey].value;
+      } else {
+        obj[varKey] = null;
+      }
+    });
+    return obj;
+  });
 };
-
 
 module.exports = {
-    getNewsLetterByAgendaId,
-    getAgendaWhereisMostRecentAndFinal
+  getNewsLetterByAgendaId,
+  getAgendaWhereisMostRecentAndFinal,
 };
-
-
-
-
